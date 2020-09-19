f9f4de479966e64231f453786acf55bb6660afde
Ildar Abdullin <ildar.abdullin@okta.com>
2020-09-18 17:45:09 -0700

    SPM

--- Package.swift
+++ Package.swift
@@ -1,18 +1,21 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "OktaOidc",
     platforms: [
-        .macOS(.v10_10), .iOS(.v11)
+        .macOS(.v10_11),
+        .iOS(.v10),
     ],
     products: [
-        .library(name: "OktaOidc", targets: ["OktaOidc"])
+        .library(
+            name: "OktaOidc",
+            targets: ["OktaOidc"]),
     ],
     targets: [
-        .target(name: "OktaOidc", dependencies: []),
-        .testTarget(name: "OktaTests", dependencies: ["OktaOidc"])
-    ]
+        .target(
+            name: "OktaOidc",
+            dependencies: [],
+            path: "Okta")
+    ],
+    swiftLanguageVersions: [.v5]
 )
